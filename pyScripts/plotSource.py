d311c32252689b11f043b2fc40a98223ff737006
yuval <yuval@bar-ilan>
2015-05-13 12:07:01 +0300

    plot ohad's brain + activity

--- pyScripts/plotSource.py
+++ pyScripts/plotSource.py
@@ -1,21 +1,31 @@
 # for the figure to open well run this script
 # in ipython, call it like this:   ipython --gui=wx
-from mne.datasets import sample
-from mne.minimum_norm import read_inverse_operator
-fname='ohad_raw-oct-6-meg-inv.fif'
-inv = read_inverse_operator(fname)
-print("Method: %s" % inv['methods'])
-print("fMRI prior: %s" % inv['fmri_prior'])
-print("Number of sources: %s" % inv['nsource'])
-print("Number of channels: %s" % inv['nchan'])
-lh_points = inv['src'][0]['rr']
-lh_faces = inv['src'][0]['use_tris']
-rh_points = inv['src'][1]['rr']
-rh_faces = inv['src'][1]['use_tris']
-from mayavi import mlab
-mlab.figure(size=(600, 600), bgcolor=(0, 0, 0))
-mlab.triangular_mesh(lh_points[:, 0], lh_points[:, 1], lh_points[:, 2],
-                     lh_faces)
-mlab.triangular_mesh(rh_points[:, 0], rh_points[:, 1], rh_points[:, 2],
-                     rh_faces)
+
+#taken from http://pysurfer.github.io/auto_examples/plot_meg_inverse_solution.html
+
+import numpy as np
+from surfer import Brain, TimeViewer
+from surfer.io import read_stc
+
+
+brain = Brain('fsaverage', 'both', 'pial', views='caudal')
+stc = read_stc('fsaverage_wbw-meg-lh.stc')
+data = stc['data']
+vertices = stc['vertices']
+time = 1e3 * np.linspace(stc['tmin'], stc['tmin'] + data.shape[1] * stc['tstep'], data.shape[1])
+colormap = 'hot'
+time_label = 'time=%0.2f ms'
+brain.add_data(data, colormap=colormap, vertices=vertices, smoothing_steps=10,
+               time=time, time_label=time_label, hemi='lh')
+
+stc = read_stc('fsaverage_wbw-meg-rh.stc')
+data = stc['data']
+vertices = stc['vertices']
+brain.add_data(data, colormap=colormap, vertices=vertices, smoothing_steps=10,
+               time=time, time_label=time_label, hemi='rh')
+#brain.set_data_time_index(2)
+#brain.scale_data_colormap(fmin=13, fmid=18, fmax=22, transparent=True)
+viewer = TimeViewer(brain)
+
+
 
---
+++ pyScripts/plotSourceSpace.py
@@ -0,0 +1,23 @@
+
+### plot the source space
+# for the figure to open well run this script
+# in ipython, call it like this:   ipython --gui=wx
+from mne.datasets import sample
+from mne.minimum_norm import read_inverse_operator
+fname='ohad_raw-oct-6-meg-inv.fif'
+inv = read_inverse_operator(fname)
+print("Method: %s" % inv['methods'])
+print("fMRI prior: %s" % inv['fmri_prior'])
+print("Number of sources: %s" % inv['nsource'])
+print("Number of channels: %s" % inv['nchan'])
+lh_points = inv['src'][0]['rr']
+lh_faces = inv['src'][0]['use_tris']
+rh_points = inv['src'][1]['rr']
+rh_faces = inv['src'][1]['use_tris']
+from mayavi import mlab
+mlab.figure(size=(600, 600), bgcolor=(0, 0, 0))
+mlab.triangular_mesh(lh_points[:, 0], lh_points[:, 1], lh_points[:, 2],
+                     lh_faces)
+mlab.triangular_mesh(rh_points[:, 0], rh_points[:, 1], rh_points[:, 2],
+                     rh_faces)
+

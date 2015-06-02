bb929a28beafeae7cd47e7c1e8bfdebefdc6ad5c
yuval-harpaz <yuvharpaz@gmail.com>
2015-06-02 16:33:40 +0300

    lcmv statistics

--- pyScripts/lcmv1morph.py
+++ pyScripts/lcmv1morph.py
@@ -7,11 +7,9 @@ from termcolor import colored
 subjects = ['idan', 'inbal', 'liron', 'maor', 'mark', 'ohad', 'odelia', 'yoni'];
 vertices_to = [np.arange(10242), np.arange(10242)]
 for sub in subjects:
-    #X=[]
     fsSub='alice'+sub[0].upper()+sub[1:]
     cd('/home/yuval/Copy/MEGdata/alice/'+sub+'/MNE')
     stc_orig=mne.read_source_estimate('lcmv_orig')
-    # stcF = lcmv(evoked1, fwdFixed, noise_cov, data_cov, reg=0.01,pick_ori='normal')
     vertices=np.arange(10242)
     #stc = mne.morph_data(fsSub, 'fsaverage', stc_orig, grade=5,subjects_dir='/usr/local/freesurfer/subjects') # beware with n_jobs on PyCharm
     if  isfile('morph_mat.p'):
@@ -22,5 +20,4 @@ for sub in subjects:
         morph_mat = mne.compute_morph_matrix(fsSub, 'fsaverage', stc_orig.vertno, vertices_to, subjects_dir='/usr/local/freesurfer/subjects')
         pickle.dump(morph_mat,open('morph_mat.p','w'))
     stc_fsa = mne.morph_data_precomputed(fsSub, 'fsaverage', stc_orig, vertices_to, morph_mat)
-    #stc_to = mne.morph_data(subject_from, 'fsaverage', stc, n_jobs=1, grade=vertices_to)
     stc_fsa.save('lcmv_fsa')
---
+++ pyScripts/lcmv_ttest.py
@@ -0,0 +1,73 @@
+# ipython --gui=wx
+from os import chdir
+from mne import read_source_estimate
+from scipy.stats import ttest_1samp as ttest
+from surfer import Brain
+import numpy as np
+
+%gui
+%gui wx
+
+filePref='lcmv_fsa'
+time0=0.0875
+time1=0.1125
+const=2 #ttest_1samp(X,constant)
+
+subjects = ['idan', 'inbal', 'liron', 'maor', 'mark', 'ohad', 'odelia', 'yoni'];
+counter=0
+XX=np.empty([8,20484])
+for sub in subjects:
+    X=[]
+    #fsSub='alice'+sub[0].upper()+sub[1:]
+    chdir('/home/yuval/Copy/MEGdata/alice/'+sub+'/MNE')
+    stc = read_source_estimate(filePref)
+    samp0=np.abs(stc.times - time0).argmin()
+    samp1=np.abs(stc.times - time1).argmin()
+    X=stc.data[0:20484,samp0:samp1]
+    Xm=X.mean(1)
+    XX[counter,0:20484]=Xm
+    counter+=1
+XXm=XX.mean(0)
+XXmL=XXm[0:10242]
+XXmR=XXm[10242:20484]
+
+brain = Brain('fsaverage', 'both', 'pial', views='caudal', subjects_dir = '/usr/local/freesurfer/subjects')
+brain.add_data(XXmL, colormap='hot', vertices=np.arange(10242), smoothing_steps=10, hemi='lh')
+brain.add_data(XXmR, colormap='hot', vertices=np.arange(10242), smoothing_steps=10, hemi='rh')
+brain.scale_data_colormap(fmin=0, fmid=XXm.max()/2, fmax=XXm.max(), transparent=True)
+# statistics
+results=ttest(XX,const) # results: row 0 = t, raw 1 = p
+
+posP=results[1]
+posP[np.where(results[0]<=0)]=1
+notSig=np.where(posP<=0.05) # one tailed, positive t only
+
+posT=results[0]
+posT[notSig]=0
+#posT[np.where(results[0]<0)]=0
+#posT[np.where(results[1]>=0.1)]=0
+brain1 = Brain('fsaverage', 'both', 'pial', views='caudal', subjects_dir = '/usr/local/freesurfer/subjects')
+brain1.add_data(posT[0:10242], colormap='hot', vertices=np.arange(10242), smoothing_steps=10, hemi='lh')
+brain1.add_data(posT[10242:20484], colormap='hot', vertices=np.arange(10242), smoothing_steps=10, hemi='rh')
+maxT=posT.max()
+brain1.scale_data_colormap(fmin=0, fmid=maxT/2, fmax=maxT, transparent=True)
+
+
+
+
+
+#if  os.path.isfile('mne_dSPM_inverse-lh.stc'):
+#        print colored(sub,'red'),colored('/MNE/mne_dSPM_inverse-lh.stc exists','blue')
+#    else:
+#        print colored('working on '+sub,'blue')
+#        fname_inv = sub+'_raw-oct-6-meg-inv.fif'
+#        if os.path.isfile(fname_inv):
+#            print('okay')
+#        else:
+#            print colored(sub+' no inv!!!','red')
+#        fname_evoked=sub+'ftWbW-ave.fif'
+#        evoked = read_evokeds(fname_evoked, condition=0, baseline=(None, 0))
+#        inverse_operator = read_inverse_operator(fname_inv)
+#        stc = apply_inverse(evoked, inverse_operator, lambda2, method, pick_ori=None)
+#        stc.save('mne_%s_inverse' % method)
+    

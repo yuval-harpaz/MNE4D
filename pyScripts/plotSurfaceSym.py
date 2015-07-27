927864c1e7ae1043cf9b307c28dcfc3713c980ee
yuval-harpaz <yuvharpaz@gmail.com>
2015-07-27 08:21:23 +0300

    gitUpdate

--- pyScripts/plotSurfaceSym.py
+++ pyScripts/plotSurfaceSym.py
@@ -1,11 +1,11 @@
-
-
-
-%gui
-%gui wx
+import pickle
 from surfer import Brain
 from scipy.io import loadmat
 import numpy as np
+
+%gui
+%gui wx
+
 brain = Brain('fsaverage_sym', 'both', 'pial', views='caudal', subjects_dir = '/usr/local/freesurfer/subjects')
 cd /home/yuval/Copy/MEGdata/alice/idan/MNE
 mat = loadmat('R100.mat')
@@ -18,3 +18,9 @@ vertices[:]=verticesL
 brain.add_data(R[0:10242], colormap='hot', vertices=vertices, smoothing_steps=10, hemi='lh')
 vertices[:]=verticesR
 brain.add_data(R[10242:20484], colormap='hot', vertices=vertices, smoothing_steps=10, hemi='rh')
+
+
+morph_mat = pickle.load( open( "morph_mat.p", "rb" ) )
+vertices_to = [np.arange(10242), np.arange(10242)]
+mne.compute_morph_matrix('aliceIdan', 'fsaverage_sym', stc_orig.vertno, vertices_to, subjects_dir='/usr/local/freesurfer/subjects')
+pickle.dump(morph_mat,open('morph_mat.p','w'))

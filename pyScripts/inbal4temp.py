0f2b9068f71985446dcb7f9d193cb88436a9e3b4
yuval-harpaz <yuvharpaz@gmail.com>
2015-11-02 14:09:20 +0200

    compare real brain with template

--- pyScripts/inbal4temp.py
+++ pyScripts/inbal4temp.py
@@ -3,6 +3,7 @@ __author__ = 'yuval'
 import mne
 from os import environ
 from shutil import copyfile
+import surfer
 from get_env_variable import get_env_variable as ge
 # import numpy as np
 # import surfer
@@ -50,73 +51,91 @@ mne.viz.plot_bem(subject='aliceIdan',subjects_dir='/usr/local/freesurfer/subject
 mne.gui.coregistration(subject=subject,subjects_dir=subjects_dir,inst='inbal4temp-raw.fif')
 
 
+# filter
+raw=mne.io.read_raw_fif('inbal4temp-raw.fif',preload=True,)
+raw.pick_types('mag')
+raw.filter(1,40)
+# make epochs and compute noise covariance
+events=mne.read_events('4-eve.fif')
+event_id = dict(beep=100)
+epochs = mne.Epochs(raw, events, event_id, tmin=-0.1, tmax=0.3, baseline=(None, 0), preload=True)
+noise_cov = mne.compute_covariance(epochs, tmax=0., method=['shrunk', 'empirical'])
+fig_cov, fig_spectra = mne.viz.plot_cov(noise_cov, raw.info)
 
-raw=mne.io.read_raw_fif('idan_raw.fif')
-info = raw.info
+aud = epochs['beep'].average()
+# butterfly plot
+aud.plot()
+# topography plot
+aud.plot_topomap(times=(0.092, 0.135, 0.2), ch_type='mag')
+
+# make forward solution
+src = mne.setup_source_space(subject, subjects_dir=subjects_dir, add_dist=False, overwrite=True)
+trans = 'inbal4temp-trans.fif'
+bem=subjects_dir+'/'+subject+'/bem/'+subject+'-bem.fif'
+
+# Note that forward solutions can also be read with read_forward_solution
+fwd = mne.make_forward_solution(raw.info, trans, src, bem, fname='inbal4temp-fwd.fif', meg=True, eeg=False, mindist=5.0, n_jobs=2, overwrite=True)
+
+inv=mne.minimum_norm.make_inverse_operator(raw.info,fwd,noise_cov)
+mne.minimum_norm.write_inverse_operator('inbal4temp-inv.fif',inv)
+snr = 3.0
+lambda2 = 1.0 / snr ** 2
+method = "dSPM"
+stc = mne.minimum_norm.apply_inverse(aud, inv, lambda2, method, pick_ori=None)
 
 
+# plot the subject's brain with pysurfer
+% gui
+% gui wx
+time=0.092
+tidx = abs(aud.times - time).argmin()
+stcR = stc.rh_data[:,tidx]
+stcL = stc.lh_data[:,tidx]
+verticesR = stc.rh_vertno
+verticesL = stc.lh_vertno
+colormap = 'hot'
+# export SUBJECTS_DIR=/usr/local/freesurfer/subjects/
+brain = surfer.Brain(subject, 'both', 'white', views='caudal',subjects_dir=subjects_dir)
+brain.add_data(stcL, colormap=colormap, vertices=verticesL, smoothing_steps=10,  hemi='lh')
+brain.add_data(stcR, colormap=colormap, vertices=verticesR, smoothing_steps=10, hemi='rh')
+maxval=max(stcL.max(),stcR.max())
+brain.scale_data_colormap(fmin=0, fmid=maxval/2, fmax=maxval, transparent=True)
+
+stc.plot(subject=subject,surface='pial',hemi='both',colormap='hot',smoothing_steps=10,transparent=True,subjects_dir=subjects_dir,time_viewer=True)
 
 
-# evoked = mne.read_evokeds('4aud-ave.fif', condition=0, baseline=(-0.2, 0))
-# #inverse_operator = mne.minimum_norm.read_inverse_operator('4_raw-oct-6-meg-inv.fif')
-# inverse_operator = mne.minimum_norm.read_inverse_operator('4temp-meg-inv.fif')
-# snr = 3.0
-# lambda2 = 1.0 / snr ** 2
-# method = "dSPM"
-# stc = mne.minimum_norm.apply_inverse(evoked, inverse_operator, lambda2, method, pick_ori=None)
-# stc.save('4tempaud_dSPM')
-#
-# # plot the subject's brain with pysurfer
-# % gui
-# % gui wx
-#
-#
-# # adding functional data
-# stcR = surfer.io.read_stc('4tempaud_dSPM-rh.stc')
-# dataR = stcR['data']
-# verticesR = stcR['vertices']
-# stcL = surfer.io.read_stc('4tempaud_dSPM-lh.stc')
-# dataL = stcL['data']
-# verticesL = stcL['vertices']
-# time = 1e3 * np.linspace(stcL['tmin'], stcL['tmin'] + dataL.shape[1] * stcL['tstep'], dataL.shape[1])
-# colormap = 'hot'
-# time_label = 'time=%0.2f ms'
-# # export SUBJECTS_DIR=/usr/local/freesurfer/subjects/
-# brain = surfer.Brain('inbal4temp', 'both', 'white', views='caudal',subjects_dir='/usr/local/freesurfer/subjects/')
-# brain.add_data(dataL, colormap=colormap, vertices=verticesL, smoothing_steps=10, time=time, time_label=time_label, hemi='lh')
-# brain.add_data(dataR, colormap=colormap, vertices=verticesR, smoothing_steps=10, time=time, time_label=time_label, hemi='rh')
-# brain.set_data_time_index(388)
-# brain.scale_data_colormap(fmin=0, fmid=10, fmax=20, transparent=True)
-# viewer = surfer.TimeViewer(brain)
-#
-# brain.set_data_time_index(433)
-#
-#
-# # morph, move to data to fsaverage (Talairach)
-# # make the transformation matrix. this takes a few min, save for later use.
-# vertices_to = [np.arange(10242), np.arange(10242)]
-# morph_mat = mne.compute_morph_matrix('inbal4', 'fsaverage', stc.vertno, vertices_to, subjects_dir='/usr/local/freesurfer/subjects')
-# print("done morphing")
-# pickle.dump(morph_mat,open('morph_mat.p','w'))
-# # you can read it like this:
-# # morph_mat = pickle.load( open( "morph_mat.p", "rb" ) )
-# stc_fsa = mne.morph_data_precomputed('inbal4', 'fsaverage', stc, vertices_to, morph_mat)
-#
-#
-# dataL = stc_fsa.lh_data
-# dataR = stc_fsa.rh_data
-# verticesL = stc_fsa.lh_vertno;
-# verticesR = stc_fsa.rh_vertno;
-#
-# time = 1e3 * np.linspace(stcL['tmin'], stcL['tmin'] + dataL.shape[1] * stcL['tstep'], dataL.shape[1])
-# colormap = 'hot'
-# time_label = 'time=%0.2f ms'
-# # export SUBJECTS_DIR=/usr/local/freesurfer/subjects/
-# brain = surfer.Brain('fsaverage', 'both', 'pial', views='caudal',subjects_dir='/usr/local/freesurfer/subjects/')
-# brain.add_data(dataL, colormap=colormap, vertices=verticesL, smoothing_steps=10, time=time, time_label=time_label, hemi='lh')
-# brain.add_data(dataR, colormap=colormap, vertices=verticesR, smoothing_steps=10, time=time, time_label=time_label, hemi='rh')
-# brain.set_data_time_index(388)
-# brain.scale_data_colormap(fmin=0, fmid=4, fmax=8, transparent=True)
-# viewer = surfer.TimeViewer(brain)
-#
-# brain.set_data_time_index(433)
+cd ../4/
+subject='inbal4'
+srcOrig = mne.setup_source_space(subject=subject, subjects_dir=subjects_dir, add_dist=False, overwrite=True)
+transOrig = '4_raw-trans.fif'
+bemOrig=subjects_dir+'/'+subject+'/bem/'+subject+'-5120-bem-sol.fif'
+
+# Note that forward solutions can also be read with read_forward_solution
+fwdOrig = mne.make_forward_solution(raw.info, transOrig, srcOrig, bemOrig, meg=True, eeg=False, mindist=5.0, n_jobs=2, overwrite=True)
+
+invOrig=mne.minimum_norm.make_inverse_operator(raw.info,fwdOrig,noise_cov)
+#mne.minimum_norm.write_inverse_operator('inbal4temp-inv.fif',inv)
+snr = 3.0
+lambda2 = 1.0 / snr ** 2
+method = "dSPM"
+stcOrig = mne.minimum_norm.apply_inverse(aud, invOrig, lambda2, method, pick_ori=None)
+
+
+# plot the subject's brain with pysurfer
+% gui
+% gui wx
+time=0.092
+tidx = abs(aud.times - time).argmin()
+stcR = stcOrig.rh_data[:,tidx]
+stcL = stcOrig.lh_data[:,tidx]
+verticesR = stcOrig.rh_vertno
+verticesL = stcOrig.lh_vertno
+colormap = 'hot'
+# export SUBJECTS_DIR=/usr/local/freesurfer/subjects/
+brainO = surfer.Brain(subject, 'both', 'white', views='caudal',subjects_dir=subjects_dir)
+brainO.add_data(stcL, colormap=colormap, vertices=verticesL, smoothing_steps=10,  hemi='lh')
+brainO.add_data(stcR, colormap=colormap, vertices=verticesR, smoothing_steps=10, hemi='rh')
+maxval=max(stcL.max(),stcR.max())
+brainO.scale_data_colormap(fmin=0, fmid=maxval/2, fmax=maxval, transparent=True)
+
+stcOrig.plot(subject=subject,surface='pial',hemi='both',colormap='hot',smoothing_steps=10,transparent=True,subjects_dir=subjects_dir,time_viewer=True)
